30ee2b3590d55576974c4ec28d3028f1dceabe07
flefur <f.lefur@djust.io>
2023-09-15 14:51:21 +0200

    Remove unused code and add error messages

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -6,46 +6,55 @@ import Global from '../types/index'
 const router = express.Router();
 
 router.get('/flights', async (req: Request, res: Response) => {
-  const currency: string = req.query.currency as string;
-  const date: string = req.query.date as string;
+    const currency: string = req.query.currency as string;
+    const date: string = req.query.date as string;
 
-  if (currency === undefined) {
-    res.status(400);
-    res.send({
-      'code': 'CURRENCY_IS_MANDATORY',
-      'message': 'Currency must be specified in the query'
-    });
-    return;
-  }
+    if (currency === undefined) {
+        res.status(400);
+        res.send({
+            'code': 'CURRENCY_IS_MANDATORY',
+            'message': 'Currency must be specified in the query'
+        });
+        return;
+    }
 
-  if (date === undefined) {
-    res.status(400);
-    res.send({
-      'code': 'DATE_IS_MANDATORY',
-      'message': 'Date must be specified in the query'
-    });
-    return;
-  }
+    if (date === undefined) {
+        res.status(400);
+        res.send({
+            'code': 'DATE_IS_MANDATORY',
+            'message': 'Date must be specified in the query'
+        });
+        return;
+    }
 
-  try {
-    const params = { params: {
-        currency: currency,
-        date: date
-    }}
-    const response = await axios.get(Global.FLIGHT_API + "/flights", params);
-    res.send(response.data);
-  } catch (e) {
-    res.send({
-      'code': 'ERROR',
-      'message': e
-    });
-  }
+    try {
+        const params = { params: {
+            currency: currency,
+            date: date
+        }}
+        const response = await axios.get(Global.FLIGHT_API + "/flights", params);
+        res.send(response.data);
+    } catch (exception) {
+        console.log(exception);
+        res.send({
+            'code': 'COULD_NOT_RETRIEVE_FLIGHTS',
+            'message': 'Could not retrieve flights'
+        });
+    }
 });
 
 
 router.get('/flights/currencies', async (req: Request, res: Response) => {
-    const response = await axios.get(Global.FLIGHT_API + "/flights/currencies");
-    res.send(response.data);
-  });
+    try {
+        const response = await axios.get(Global.FLIGHT_API + "/flights/currencies");
+        res.send(response.data);
+    } catch (exception) {
+        console.log(exception);
+        res.send({
+            'code': 'COULD_NOT_RETRIEVE_CURRENCIES',
+            'message': 'Could not retrieve currencies'
+          });
+    }
+});
 
 export default router;
