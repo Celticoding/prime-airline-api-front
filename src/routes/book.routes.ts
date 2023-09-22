366bbecff96890f4ee64c682eebf65342bee00b2
flefur <f.lefur@djust.io>
2023-09-22 09:58:07 +0200

    Fix bookingHistory

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -7,9 +7,12 @@ const router = express.Router();
 router.post('/book', async (req: Request, res: Response) => {
 
   try {
+    console.log(global.BOOKING_API + "/book")
     const response = await axios.post(global.BOOKING_API + "/book", {book: req.body});
+    console.log(response);
     res.send(response.data);
   } catch (e) {
+    console.log(e)
     res.send({
       'code': 'ERROR',
       'message': 'BOOKING_API unreachable'
@@ -19,7 +22,12 @@ router.post('/book', async (req: Request, res: Response) => {
 
 router.get('/booking-history', async (req: Request, res: Response) => {
   try {
-    const response = await axios.post(global.BOOKING_API + "/booking-history");
+    if (req.query.userId === undefined) {
+      res.sendStatus(400)
+      return
+    }
+
+    const response = await axios.get(global.BOOKING_API + "/booking-history", { params: { userId: req.query.userId } });
     res.send(response.data);
   } catch (e) {
     res.send({
@@ -29,5 +37,16 @@ router.get('/booking-history', async (req: Request, res: Response) => {
   }
 });
 
+router.post('/cancel-book', async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post(global.QUEUE_API + "/cancel-book", {bookingId: req.body});
+    res.send(response.data);
+  } catch (e) {
+    res.send({
+      'code': 'ERROR',
+      'message': 'QUEUE_API unreachable'
+    });
+  }
+});
 
 export default router;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
 declare global {
     var FLIGHT_API: string;
     var BOOKING_API: string;
+    var QUEUE_API: string;
 }
 
 global.FLIGHT_API = "http://localhost:8081"
 global.BOOKING_API = "http://localhost:8082"
+global.QUEUE_API = "http://localhost:8085"
   
 export default global;
